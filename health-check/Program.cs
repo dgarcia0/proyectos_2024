ab0ed7f107a1f9bc54be109e1880ddffe123df74
David <davidgarcia227@gmail.com>
2024-08-03 18:23:27 +0200

    added ICMP check

--- health-check/Program.cs
+++ health-check/Program.cs
@@ -1,8 +1,13 @@
+using health_check;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-builder.Services.AddHealthChecks();
+builder.Services.AddHealthChecks()
+    .AddCheck("ICMP_01", new ICMPHealthCheck("www.ryadel.com", 100))
+    .AddCheck("ICMP_02", new ICMPHealthCheck("www.google.com", 100))
+    .AddCheck("ICMP_03", new ICMPHealthCheck("www.does-not-exist.com", 100));
 
 var app = builder.Build();
 
